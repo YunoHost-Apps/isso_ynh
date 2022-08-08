b6a731fd159c27a7e16b0de43842a68bfcf7f138
navanchauhan <navanchauhan@gmail.com>
2022-08-07 23:44:02 -0400

    fix dep

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app (must be on a single line)
-pkg_dependencies="git curl python3 python3-pip python3-venv libpq-dev libsasl2-dev python3-dev libssl-dev"
+pkg_dependencies="git curl python3 python3-pip python3-venv libpq-dev libsasl2-dev python3-dev libssl-dev libffi-dev python3-cffi"
 
 #=================================================
 # PERSONAL HELPERS
