973a32bdfb98af64babc2d907caff539744245d1
navanchauhan <navanchauhan@gmail.com>
2022-08-07 23:24:57 -0400

    add dependencies

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app (must be on a single line)
-pkg_dependencies="python3-setuptools python3-virtualenv python3-dev python3-venv"
+pkg_dependencies="python3-setuptools python3-virtualenv python3-dev python3-venv python3-pip python3-dev"
 
 #=================================================
 # PERSONAL HELPERS
