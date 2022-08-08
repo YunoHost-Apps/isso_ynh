1c5d7361a027b0b915e8489b4d33c7cf58425e5b
navanchauhan <navanchauhan@gmail.com>
2022-08-07 23:16:56 -0400

    add python3-venv dep

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app (must be on a single line)
-pkg_dependencies="python3-setuptools python3-virtualenv python3-dev"
+pkg_dependencies="python3-setuptools python3-virtualenv python3-dev python3-venv"
 
 #=================================================
 # PERSONAL HELPERS
